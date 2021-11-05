a9a70987844ca9c12e80aa75ce54840ce909c64b
Christopher Biscardi <chris@christopherbiscardi.com>
2021-11-05 01:52:38 -0700

    booleans

--- crates/pokemon-api/src/main.rs
+++ crates/pokemon-api/src/main.rs
@@ -33,6 +33,7 @@ async fn main() -> Result<(), Error> {
 struct PokemonHp {
     name: String,
     hp: u16,
+    legendary_or_mythical: bool,
 }
 
 #[instrument]
@@ -66,7 +67,16 @@ async fn handler(
             info!(pokemon_name,"requested a pokemon");
             let result = sqlx::query_as!(
                     PokemonHp,
-                    r#"SELECT name, hp FROM pokemon WHERE slug = ?"#,
+                    r#"
+SELECT
+    name,
+    hp,
+    legendary_or_mythical as "legendary_or_mythical!: bool"
+FROM
+    pokemon
+WHERE
+    slug = ?
+"#,
                     pokemon_name
                 )
                 .fetch_one(POOL.get().unwrap())
@@ -124,7 +134,8 @@ mod tests {
                 body: Some(Body::Text(
                     serde_json::to_string(&PokemonHp {
                         name: String::from("Bulbasaur"),
-                        hp: 45
+                        hp: 45,
+                        legendary_or_mythical: false
                     },)
                     .unwrap()
                 )),
